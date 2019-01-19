aee9cd2d23b233ac435816960f5a0ecae877961a
Jenny Chang <jennychang@jennys-macbook-pro.local>
2019-01-19 13:34:21 -0800

    Initial Commit

---
+++ BillSplitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BillSplitter
+//
+//  Created by Jenny Chang on 19/01/2019.
+//  Copyright © 2019 Jenny Chang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
